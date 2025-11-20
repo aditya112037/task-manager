d166ed50ee0ef5fda5fd5104249fc66428f88cdd
addy <aditya.agrawal112037@gmail.com>
2025-11-20 18:23:56 +0530

    Fix api.js and API URL

--- task-manager-frontend/src/services/api.js
+++ task-manager-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Load backend URL from Vercel environment variable
-const API = process.env.REACT_APP;
+const API = process.env.REACT_APP_API_URL;
 
 // Create axios instance
 const api = axios.create({
