22624565a9449b270e60c17f1de9ce4d02a4df7e
addy <aditya.agrawal112037@gmail.com>
2025-11-24 15:33:26 +0530

    update

--- task-manager-backend/utils/sendEmail.js
+++ task-manager-backend/utils/sendEmail.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.BREVO_SMTP_HOST,
   port: process.env.BREVO_SMTP_PORT,
+  secure: true,
   auth: {
     user: process.env.BREVO_SMTP_LOGIN,
     pass: process.env.BREVO_SMTP_PASSWORD,
