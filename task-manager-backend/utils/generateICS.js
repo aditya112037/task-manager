660ec0b44d687120dcbd55b64eeb0d97a5dcf2d0
addy <aditya.agrawal112037@gmail.com>
2025-11-25 13:46:49 +0530

    task-final

--- task-manager-backend/utils/generateICS.js
+++ task-manager-backend/utils/generateICS.js
@@ -8,46 +8,58 @@ function format(date) {
   return d.toISOString().replace(/[-:]/g, "").split(".")[0] + "Z";
 }
 
+// Escape ICS unsafe characters
+function escapeICS(str = "") {
+  return String(str)
+    .replace(/\\/g, "\\\\")
+    .replace(/;/g, "\\;")
+    .replace(/,/g, "\\,")
+    .replace(/\n/g, "\\n");
+}
+
 function generateICS(task) {
+  const title = escapeICS(task.title || "Task");
+  const description = escapeICS(task.description || "");
+
   const startDate = task.dueDate || Date.now();
-  const endDate = task.dueDate || Date.now();
+  const endDate = new Date(startDate).getTime() + 30 * 60 * 1000; // +30 mins
 
-  return `
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//TaskManager//EN
-CALSCALE:GREGORIAN
-METHOD:PUBLISH
+  return [
+    "BEGIN:VCALENDAR",
+    "VERSION:2.0",
+    "CALSCALE:GREGORIAN",
+    "METHOD:PUBLISH",
 
-BEGIN:VEVENT
-UID:${task._id}
-DTSTAMP:${format(Date.now())}
-DTSTART:${format(startDate)}
-DTEND:${format(endDate)}
-SUMMARY:${task.title}
-DESCRIPTION:${task.description || ""}
+    "BEGIN:VEVENT",
+    `UID:${task._id}`,
+    `DTSTAMP:${format(Date.now())}`,
+    `DTSTART:${format(startDate)}`,
+    `DTEND:${format(endDate)}`,
+    `SUMMARY:${title}`,
+    `DESCRIPTION:${description}`,
 
-BEGIN:VALARM
-TRIGGER:-PT10M
-ACTION:DISPLAY
-DESCRIPTION:Reminder - ${task.title}
-END:VALARM
+    // alarms
+    "BEGIN:VALARM",
+    "TRIGGER:-PT10M",
+    "ACTION:DISPLAY",
+    `DESCRIPTION:Reminder - ${title}`,
+    "END:VALARM",
 
-BEGIN:VALARM
-TRIGGER:-PT1H
-ACTION:DISPLAY
-DESCRIPTION:Reminder - ${task.title}
-END:VALARM
+    "BEGIN:VALARM",
+    "TRIGGER:-PT1H",
+    "ACTION:DISPLAY",
+    `DESCRIPTION:Reminder - ${title}`,
+    "END:VALARM",
 
-BEGIN:VALARM
-TRIGGER:-P1D
-ACTION:DISPLAY
-DESCRIPTION:Reminder - ${task.title}
-END:VALARM
+    "BEGIN:VALARM",
+    "TRIGGER:-P1D",
+    "ACTION:DISPLAY",
+    `DESCRIPTION:Reminder - ${title}`,
+    "END:VALARM",
 
-END:VEVENT
-END:VCALENDAR
-  `;
+    "END:VEVENT",
+    "END:VCALENDAR",
+  ].join("\r\n");  // VERY IMPORTANT
 }
 
 module.exports = generateICS;
