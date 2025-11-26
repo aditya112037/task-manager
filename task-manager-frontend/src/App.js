53a54ab813984218af650bfdfaf951551e475e8f
addy <aditya.agrawal112037@gmail.com>
2025-11-26 16:35:12 +0530

    admin backend+frontend

--- task-manager-frontend/src/App.js
+++ task-manager-frontend/src/App.js
@@ -5,7 +5,7 @@ import Layout from './components/Layout/Layout';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import TaskList from './components/Task/TaskList';
-import OAuthSuccess from './Auth/OAuthSuccess';
+import OAuthSuccess from './components/Auth/OAuthSuccess';
 import Teams from "./pages/Teams";
 import JoinTeam from "./pages/JoinTeam";
 import Dashboard from "./pages/Dashboard";
