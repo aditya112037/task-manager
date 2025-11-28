188136468d5ae77153442035aaeced7d7f2b6fcc
addy <aditya.agrawal112037@gmail.com>
2025-11-28 11:54:45 +0530

    create team

--- task-manager-frontend/src/services/teamTasksAPI.js
+++ task-manager-frontend/src/services/teamTasksAPI.js
@@ -1,4 +1,9 @@
-getTasks(teamId)
-createTask(teamId, data)
-updateTask(taskId, data)
-deleteTask(taskId)
+// src/services/teamTasksAPI.js
+import api from "./api";
+
+export const teamTasksAPI = {
+  getTasks: (teamId) => api.get(`/api/team-tasks/${teamId}`),
+  createTask: (teamId, data) => api.post(`/api/team-tasks/${teamId}`, data),
+  updateTask: (taskId, data) => api.put(`/api/team-tasks/${taskId}`, data),
+  deleteTask: (taskId) => api.delete(`/api/team-tasks/${taskId}`),
+};
--- task-manager-frontend/src/services/teamsAPI.js
+++ task-manager-frontend/src/services/teamsAPI.js
@@ -1,17 +1,9 @@
+// src/services/teamsAPI.js
 import api from "./api";
 
-// ---- TEAM CRUD ----
 export const teamsAPI = {
   getTeams: () => api.get("/api/teams/my"),
   createTeam: (data) => api.post("/api/teams", data),
   getTeam: (teamId) => api.get(`/api/teams/${teamId}/details`),
   joinTeam: (teamId) => api.post(`/api/teams/${teamId}/join`),
-};
-
-// ---- TEAM TASKS CRUD ----
-export const teamTasksAPI = {
-  getTasks: (teamId) => api.get(`/api/team-tasks/${teamId}`),
-  createTask: (teamId, data) => api.post(`/api/team-tasks/${teamId}`, data),
-  updateTask: (taskId, data) => api.put(`/api/team-tasks/${taskId}`, data),
-  deleteTask: (taskId) => api.delete(`/api/team-tasks/${taskId}`),
 };
