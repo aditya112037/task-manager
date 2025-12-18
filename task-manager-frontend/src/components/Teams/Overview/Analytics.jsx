4a29cefd96383b9decc6ea02ba0319fcf697437d
addy <aditya.agrawal112037@gmail.com>
2025-12-18 14:25:18 +0530

    overview

---
+++ task-manager-frontend/src/components/Teams/Overview/Analytics.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Grid } from "@mui/material";
+
+import {
+  getTaskStats,
+  getWorkloadByMember,
+  getDeliveryHealth,
+  getStatusDistribution,
+} from "./overview.utils";
+
+import TeamKPIs from "./TeamKPIs";
+import StatusDonut from "./StatusDonut";
+import WorkloadChart from "./WorkloadChart";
+import DeliveryHealth from "./DeliveryHealth";
+import ActivityFeed from "./ActivityFeed";
+
+const TeamAnalytics = ({ team, tasks, myRole }) => {
+  const taskStats = getTaskStats(tasks);
+  const workload = getWorkloadByMember(tasks, team.members || []);
+  const deliveryHealth = getDeliveryHealth(tasks);
+  const statusDist = getStatusDistribution(tasks);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      {/* KPI ROW */}
+      <TeamKPIs stats={taskStats} />
+
+      {/* CHARTS */}
+      <Grid container spacing={3} sx={{ mt: 1 }}>
+        <Grid item xs={12} md={4}>
+          <StatusDonut data={statusDist} />
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <WorkloadChart data={workload} />
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <DeliveryHealth data={deliveryHealth} />
+        </Grid>
+
+        {/* ACTIVITY */}
+        <Grid item xs={12}>
+          <ActivityFeed teamId={team._id} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default TeamAnalytics;
