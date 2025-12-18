71e28ab3fa71ef67e72dc4025b2163650d8e253f
addy <aditya.agrawal112037@gmail.com>
2025-12-18 17:15:14 +0530

    overview

--- task-manager-frontend/src/components/Teams/Overview/Analytics.jsx
+++ task-manager-frontend/src/components/Teams/Overview/Analytics.jsx
@@ -1,27 +1,21 @@
-import React , { useMemo } from "react";
+import React, { useMemo } from "react";
 import { Box, Grid } from "@mui/material";
 
 import {
   getTaskStats,
   getStatusDistribution,
+  getWorkloadByMember,
+  getDeliveryHealth,
+  getAtRiskTasks,
+  getActivityFeed,
 } from "./overview.utils";
-
-import { getAtRiskTasks } from "./overview.utils";
-import AtRiskPanel from "./AtRiskPanel";
-
-
-import { getActivityFeed } from "./overview.utils";
-import ActivityFeed from "./ActivityFeed";
-
-
-import { getWorkloadByMember } from "./overview.utils";
-import WorkloadChart from "./WorkloadChart";
-
-import { getDeliveryHealth } from "./overview.utils";
-import DeliveryHealth from "./DeliveryHealth";
 
 import TeamKPIs from "./TeamKPIs";
 import StatusDonut from "./StatusDonut";
+import WorkloadChart from "./WorkloadChart";
+import DeliveryHealth from "./DeliveryHealth";
+import AtRiskPanel from "./AtRiskPanel";
+import ActivityFeed from "./ActivityFeed";
 
 /*
   Team Analytics
@@ -33,83 +27,55 @@ import StatusDonut from "./StatusDonut";
 */
 
 const TeamAnalytics = ({ team, tasks = [], myRole }) => {
-  const stats = useMemo(
-  () => getTaskStats(tasks),
-  [tasks]
-);
-const statusDist = useMemo(
-  () => getStatusDistribution(tasks),
-  [tasks]
-);
-
-const workload = useMemo(
-  () => getWorkloadByMember(tasks, team?.members || []),
-  [tasks, team?.members]
-);
-
-const deliveryHealth = useMemo(
-  () => getDeliveryHealth(tasks),
-  [tasks]
-);
-
-const atRiskTasks = useMemo(
-  () => getAtRiskTasks(tasks, 48),
-  [tasks]
-);
-
-const activities = useMemo(
-  () => getActivityFeed(tasks, 10),
-  [tasks]
-);
   const isManagerView = myRole === "admin" || myRole === "manager";
 
-
-
-
+  const stats = useMemo(() => getTaskStats(tasks), [tasks]);
+  const statusDist = useMemo(() => getStatusDistribution(tasks), [tasks]);
+  const workload = useMemo(
+    () => getWorkloadByMember(tasks, team?.members || []),
+    [tasks, team?.members]
+  );
+  const deliveryHealth = useMemo(() => getDeliveryHealth(tasks), [tasks]);
+  const atRiskTasks = useMemo(() => getAtRiskTasks(tasks, 48), [tasks]);
+  const activities = useMemo(() => getActivityFeed(tasks, 10), [tasks]);
 
   return (
     <Box sx={{ width: "100%" }}>
-      
-      {/* ================= KPI ROW ================= */}
+      {/* ================= KPIs ================= */}
       <TeamKPIs stats={stats} />
 
+      {/* ================= WORKLOAD + STATUS ================= */}
       <Grid container spacing={3} sx={{ mt: 3 }}>
-            {isManagerView && (
-            <Grid item xs={12} md={6}>
-                <WorkloadChart data={workload} />
-            </Grid>
-            )}
+        {isManagerView && (
+          <Grid item xs={12} md={6}>
+            <WorkloadChart data={workload} />
+          </Grid>
+        )}
 
-    </Grid>
-
-
-      {/* ================= STATUS DONUT ================= */}
-      <Grid container spacing={3} sx={{ mt: 3 }}>
         <Grid item xs={12} md={6}>
           <StatusDonut data={statusDist} />
         </Grid>
       </Grid>
 
-      {/* ================= DELIVERY HEALTH ================= */}
+      {/* ================= DELIVERY + AT RISK ================= */}
       {isManagerView && (
-<Grid container spacing={3} sx={{ mt: 3 }}>
-  <Grid item xs={12} md={6}>
-    <DeliveryHealth data={deliveryHealth} />
-  </Grid>
-</Grid>
+        <Grid container spacing={3} sx={{ mt: 3 }}>
+          <Grid item xs={12} md={6}>
+            <DeliveryHealth data={deliveryHealth} />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <AtRiskPanel tasks={atRiskTasks} />
+          </Grid>
+        </Grid>
       )}
-  <Grid item xs={12} md={6}>
-    <AtRiskPanel tasks={atRiskTasks} />
-  </Grid>
 
-
-    {/* ================= ACTIVITY FEED ================= */}
-<Grid container spacing={3} sx={{ mt: 3 }}>
-  <Grid item xs={12}>
-    <ActivityFeed activities={activities} />
-  </Grid>
-</Grid>
-
+      {/* ================= ACTIVITY FEED ================= */}
+      <Grid container spacing={3} sx={{ mt: 3 }}>
+        <Grid item xs={12}>
+          <ActivityFeed activities={activities} />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
