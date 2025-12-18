ea3d011f84e9f8dcabcd978320f12c5b332fda5a
addy <aditya.agrawal112037@gmail.com>
2025-12-18 14:00:19 +0530

    overview

--- task-manager-frontend/src/pages/TeamDetails.jsx
+++ task-manager-frontend/src/pages/TeamDetails.jsx
@@ -37,7 +37,7 @@ import TeamTaskItem from "../components/Teams/TeamTaskItem";
 import TeamTaskForm from "../components/Teams/TeamTaskForm";
 import { useAuth } from "../context/AuthContext";
 import { initSocket, getSocket, disconnectSocket } from "../services/socket";
-
+import TeamOverview from "../components/Teams/Overview/overview";
 /* ---------------------------------------------------
    SAFE MEMBER RESOLVER (prevents all crashes)
 --------------------------------------------------- */
@@ -719,7 +719,11 @@ export default function TeamDetails() {
       {/* OVERVIEW */}
       {tab === 0 && (
         <Paper sx={{ p: 3, borderRadius: 3 }}>
-
+          <TeamOverview
+            team={team}
+            tasks={teamTasks}
+            myRole={myRole}
+          />
 
           <Button sx={{ mt: 3 }} variant="outlined" startIcon={<ContentCopyIcon />} onClick={handleCopyInviteLink}>
             Copy Invite Link
