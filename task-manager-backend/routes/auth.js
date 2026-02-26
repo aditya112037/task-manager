a64c5b30b718c520d1765699e27d1462a8fb5ff8
addy <aditya.agrawal112037@gmail.com>
2026-02-26 13:21:16 +0530

    tinker bell meow

--- task-manager-backend/routes/auth.js
+++ task-manager-backend/routes/auth.js
@@ -10,7 +10,7 @@ const ExecutionScoreSnapshot = require("../models/ExecutionScoreSnapshot");
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
-const STALLED_DAYS = Number(process.env.STALLED_DAYS || 1);
+const STALLED_DAYS = Number(process.env.STALLED_DAYS || 2);
 
 // Generate JWT
 const generateToken = (id) => {
